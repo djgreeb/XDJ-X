fad56cf279e03a4e04b490ea0abe25bd43522d0c
Andrei <greeb-mail@mail.ru>
2025-05-12 22:40:06 +0300

    ver. 0.25

---
+++ Power Manager/MDK-ARM/RTE/_Power_manager/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Power_manager' 
+ * Target:  'Power_manager' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
